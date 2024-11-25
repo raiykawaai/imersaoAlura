c47265055bccea2e6487322fe43d8957ed9d9d41
raiykawaai <raiy.iorio@outlook.com>
2024-11-24 23:34:09 -0300

    aula5 part1

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -1,4 +1,7 @@
+import 'dotenv/config';
+import { ObjectId } from "mongodb";
 import conectarAoBanco from "../config/dbConfig.js"
+
 // Conecta ao banco de dados usando a string de conexão fornecida como variável de ambiente
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO)
 
@@ -17,4 +20,11 @@ export async function getTodosPosts() {
      const colecao = db.collection("posts");
      return colecao.insertOne(novoPost);
     }
+
+    export async function atualizarPost(id, novoPost) {
+      const db = conexao.db("imersao-instabytes");
+      const colecao = db.collection("posts");
+      const objID = ObjectId.createFromHexString(id)
+      return colecao.updateOne({_id: new ObjectId(objID)}, {$set:novoPost});
+     }
    
